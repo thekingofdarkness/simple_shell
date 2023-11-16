bc875f6b510eab3fb3a2929a591c3ec3068cfcda
abderrahman bouitchou <abderrahman4bouichou@gmail.com>
2023-11-16 02:33:47 -0800

    fist

---
+++ code.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# This is a simple Bash script that passes Betty checks
+
+# Function to print a message
+print_message() {
+    echo "Hello, world!"
+}
+
+# Main execution
+print_message
+
+exit 0
+
